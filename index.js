eb065776aa8a72e5c3bfff97603258b908085170
RehanAjiNarwindo <rhnajin@gmail.com>
2024-01-24 14:32:44 +0700

    test

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 function add(value) {
   document.getElementById("output").textContent += value;
+  if (output.textContent.length > 10) {
+    output.textContent = "error, kepanjangan";
+  }
+  output.textContent = result;
 }
 function operator(value) {
   document.getElementById("output").textContent += value;
+  if (output.textContent.length > 10) {
+    output.textContent = "error, kepanjangan";
+  }
+  output.textContent = result;
 }
 function clearOutput() {
   document.getElementById("output").textContent = "";
@@ -10,6 +18,11 @@ function clearOutput() {
 function calculate() {
   var output = document.getElementById("output");
   var result = eval(output.textContent);
+  if (result.toString().length > 10) {
+    result = "error, kepanjangan";
+  } else {
+    result = parseFloat(result.toFixed(2));
+  }
   output.textContent = result;
 }
 function persen() {
